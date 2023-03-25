479f9354effd990bb6ddae91567c297dd0f447df
Lorentz <hOTZR@users.noreply.huggingface.co>
2023-03-25 05:34:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import json
 import asyncio
 import os
 from EdgeGPT import Chatbot, ConversationStyle
-export http_proxy="http://209.141.34.72:4001"
-export https_proxy="http://209.141.34.72:4001"
+os.environ["http_proxy"] = "http://209.141.34.72:4001"
+os.environ["https_proxy"] = "http://209.141.34.72:4001"
 async def get_model_reply(prompt,style,cookies,context=[]):
     # combines the new question with a previous context
     context += [prompt]
