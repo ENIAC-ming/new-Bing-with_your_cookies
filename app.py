d91f795f584ca30b4ef6d5f6c99c60427bc14ef2
Lorentz <hOTZR@users.noreply.huggingface.co>
2023-03-26 15:44:50 +0000

    latest version, runtime error is caused by limited resource

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ import json
 import asyncio
 import os
 from EdgeGPT import Chatbot, ConversationStyle
+import re
+
+#read cookie from local file
+# with open('./cookies.json', 'r') as f:
+#     cookies = json.load(f)
+#如果你是选择读取仓库内的cookie.json文件，那么不需要再向函数内传递cookies参数
+# 也可以删去gr.Tab("Cookies"):这一界面对应的代码
 async def get_model_reply(prompt,style,cookies,context=[]):
     # combines the new question with a previous context
     context += [prompt]
@@ -10,17 +17,40 @@ async def get_model_reply(prompt,style,cookies,context=[]):
     # given the most recent context (4096 characters)
     # continue the text up to 2048 tokens ~ 8192 charaters
     bot = Chatbot(cookies=cookies)
-    raw_data = await bot.ask(prompt, conversation_style=style)
-    #await bot.close()
-    # print(raw_data)
-    response = raw_data["item"]["messages"][1]["text"]
-    context += [response]
+    prompt2='\n\n'.join(context)[:4096]
+    raw_data = await bot.ask(prompt2, conversation_style=style)
+    await bot.close()
+    #print(raw_data)
+    try:
+        try:
+            response = raw_data["item"]["messages"][1]["text"]
+        except:
+            response = raw_data["item"]["messages"][1]["adaptiveCards"][0]["body"][0]["text"]
+        response = re.sub(r'\^', '', response)
+        response = response.rstrip()
+        context += [response]
 
     # list of (user, bot) responses. We will use this format later
-    responses = [(u, b) for u, b in zip(context[::2], context[1::2])]
+        responses = [(u, b) for u, b in zip(context[::2], context[1::2])]
+        return responses, context
+    except:
+        try:
+            if raw_data["item"]["throttling"]["numUserMessagesInConversation"] > raw_data["item"]["throttling"]["maxNumUserMessagesInConversation"]:
+                response="> **Oops, I think we've reached the end of this conversation. Please reset the bot!**"
+                context += [response]
 
-    return responses, context
+                # list of (user, bot) responses. We will use this format later
+                responses = [(u, b) for u, b in zip(context[::2], context[1::2])]
+                return responses, context
+                
+        except:    
+            if raw_data["item"]["result"]["value"] == "Throttled":
+                response="> **Error: We're sorry, but you've reached the maximum number of messages you can send to Bing in a 24-hour period. Check back later!**"
+                context += [response]
 
+                # list of (user, bot) responses. We will use this format later
+                responses = [(u, b) for u, b in zip(context[::2], context[1::2])]
+                return responses, context
 # query = 'Which is the largest country by area in the world?'
 # style="precise"
 # responses, context =asyncio.run(get_model_reply(query,style,context=[]))
